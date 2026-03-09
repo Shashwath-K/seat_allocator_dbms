bb1a1552288ed13c0de0968725365a3ffe14e143
Shashwath-K <shashwathkukkunoor@outlook.com>
2026-03-09 17:57:23 +0530

    CI-01: Initial commit

---
+++ seat_allocation/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for seat_allocation project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seat_allocation.settings')
+
+application = get_wsgi_application()
