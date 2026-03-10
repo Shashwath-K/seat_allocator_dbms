1d0d1dc5608ad89ce34a3b280d9888a80c9e5bd7
Shashwath-K <shashwathkukkunoor@outlook.com>
2026-03-10 07:58:58 +0530

    CI-04: Pages views defined

---
+++ frontend/src/main.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Batches from './pages/Batches.jsx'
+import Students from './pages/Students.jsx'
+import Rooms from './pages/Rooms.jsx'
+import Allotment from './pages/Allotment.jsx'
+import RoomAllotmentDetail from './pages/RoomAllotmentDetail.jsx'
+import Database from './pages/Database.jsx'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />
+      },
+      {
+        path: "/batches",
+        element: <Batches />
+      },
+      {
+        path: "/students",
+        element: <Students />
+      },
+      {
+        path: "/rooms",
+        element: <Rooms />
+      },
+      {
+        path: "/allotment",
+        element: <Allotment />
+      },
+      {
+        path: "/allotment/:roomId",
+        element: <RoomAllotmentDetail />
+      },
+      {
+        path: "/database",
+        element: <Database />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
