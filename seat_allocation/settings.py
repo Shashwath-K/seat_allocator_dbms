08d61871be332bc090547a3a3d9818d26ccc8585
Shashwath-K <shashwathkukkunoor@outlook.com>
2026-03-23 22:31:57 +0530

    CI-05: Final Stages of refinement

--- seat_allocation/settings.py
+++ seat_allocation/settings.py
@@ -131,4 +131,10 @@ LOGOUT_REDIRECT_URL = 'login'
 
 # CORS Settings
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:5174", # In case 5173 is busy
+    "http://127.0.0.1:5174",
+]
 
