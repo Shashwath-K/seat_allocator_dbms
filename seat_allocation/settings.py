0c3a36ac81e74e078f74c296f96a9299de8e000f
Shashwath-K <shashwathkukkunoor@outlook.com>
2026-04-06 20:56:16 +0530

    Seat allocation configs updated

--- seat_allocation/settings.py
+++ seat_allocation/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'allocation',
+    'allocation.apps.AllocationConfig',
 ]
 
 MIDDLEWARE = [
